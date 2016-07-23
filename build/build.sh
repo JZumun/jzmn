26979e84b8b6f6fb50498a5a7f74711afce22c83
Rein Bulaong <rein.bulaong@voyagerinnovation.com>
2016-07-23 21:52:04 +0800

    added standalone ajax and createEl dist files

--- build/build.sh
+++ build/build.sh
@@ -3,3 +3,5 @@
 #Using npm minifier module
 minify ../src/jzmn.js -o ../dist/jzmn-basic-min.js --no-comments
 minify ../src/jzmn.js ../src/dom/dom.js ../src/dom/module-*.js -o ../dist/jzmn-dom-min.js --no-comments
+minify ../src/dom/module-createEl.js -o ../dist/jzmn-createEl-min.js --no-comments
+minify ../src/dom/module-ajax.js -o ../dist/jzmn-ajax-min.js --no-comments
