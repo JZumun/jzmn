ea743782f54b328eb8380ba9b7393efb17b0bd39
Rein Bulaong <rein.bulaong@yahoo.com>
2016-07-27 23:47:48 +0800

    added on, off, and delegate methods in module-eventListening

--- src/dom/module-eventListening.js
+++ src/dom/module-eventListening.js
@@ -7,11 +7,45 @@
 		throw new TypeError("'jzmn' undefined. Unable to add methods for Event Listening");
 	}
 
-	factory.extendFactory("dom",{
-		delegate: function(element,child,event,callback) {
-			element.addEventListener(event,function(e){
-				if (jzmn(e.target).matches(child)) { callback(e); }
-			});
+	var eventListeners = new WeakMap();
+
+	function getEventListeners(element,event) {
+		var listeners = eventListeners.get(element);
+		if (listeners) {
+			return listeners[event] ? listeners[event] : listeners[event] = [];
+		} else {
+			listeners = {};
+			listeners[event] = [];
+			eventListeners.set(element,listeners);
+			return listeners[event];
 		}
+	}
+
+	var on = function(element,event,callback) {
+		element.addEventListener(event,callback);
+		getEventListeners(element,event).push(callback);
+	}
+
+	var delegate = function(element,child,event,callback){
+		on(element,event,function(e){
+			if (jzmn(e.target).matches(child)) { callback(e); }
+		});
+	}
+
+	var off = function(element,event,callback) {
+		if (callback) {
+			element.removeEventListener(event,callback);
+			return;
+		} 
+
+		getEventListeners(element,event).forEach(function(cb){
+			element.removeEventListener(event,cb);
+		});
+	}
+
+	factory.extendFactory("dom",{
+		on: on,
+		delegate: delegate,
+		off: off
 	})
 })(jzmn);
