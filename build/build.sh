ac807a512ada882ab686bd04220219f67ff60214
Rein Bulaong <rein.bulaong@yahoo.com>
2016-07-27 21:23:31 +0800

    Removed build directory

--- build/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-#Using npm minifier module. Run from ../
-minify src/jzmn.js -o dist/jzmn.basic.min.js --no-comments
-minify src/jzmn.js src/dom/dom.js src/dom/module-*.js -o dist/jzmn.dom.min.js --no-comments
-minify src/dom/module-createEl.js -o dist/jzmn.createEl.min.js --no-comments
-minify src/dom/module-ajax.js -o dist/jzmn.ajax.min.js --no-comments
